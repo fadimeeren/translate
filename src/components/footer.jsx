7682d7ec1a23d79f112c936c2a8b31722507f2be
fadimeeren <fadime_ern135@outlook.com>
2026-01-25 20:53:11 +0300

    Updated

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="text-center mt-8 text-zinc-500 text-sm">
+      <p>Udemig tarafından geliştirildi </p>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/language-select.jsx
+++ src/components/language-select.jsx
@@ -1,5 +1,28 @@
+import { ArrowLeftRight } from "lucide-react";
+
 const LanguageSelect = () => {
-  return <div>LanguageSelect</div>;
+  return (
+    <div className="space-y-4">
+      <div className="flex flex-col items-center gap-3 lg-flex-row">
+        {/* kaynak dil */}
+        <div className="flex-1 w-full">
+          <label className="block text-sm text-zinc-300 mb-2">Kaynak Dil</label>
+        </div>
+
+        {/* değiştirme butonu */}
+        <div className="flex item-center justify-center">
+          <button className="size-10 lg-size-12 bg-zinc-700  hover:bg-zinc-600 disabled:bg-zinc-800 disabled:opacity-0 rounded-full flex justify-center items-center cursor-pointer">
+            <ArrowLeftRight />
+          </button>
+        </div>
+
+        {/* hedef dil */}
+        <div className="flex-1 w-full">
+          <label className=" block text-sm text-zinc-300 mb-2">Hedef Dil</label>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LanguageSelect;
