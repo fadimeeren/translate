0f5495db63536e126fa1d63dc4fb73adc144cc91
fadimeeren <fadime_ern135@outlook.com>
2026-01-31 17:08:56 +0300

    Updated

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,5 +1,16 @@
+import { Languages } from "lucide-react";
+
 const Button = () => {
-  return <div>Button</div>;
+  return (
+    <div className="flex justify-center mt-6">
+      <button className="relative px-3 py-3 rounded-xl font-semibold text-lg transition-all duration-300 bg-gradient-to-r from-blue-600 to-purple-600 hover:from-blue-700 hover:to-purple-700 shadow-lg hover:shadow-xl tarnsform hover:scale-105 active scale-95 disabled:cursor-not-allowed">
+        <div>
+          <Languages />
+          <span>Çevir</span>
+        </div>
+      </button>
+    </div>
+  );
 };
 
 export default Button;
--- src/components/text-container.jsx
+++ src/components/text-container.jsx
@@ -1,3 +1,5 @@
+import { ArrowRight } from "lucide-react";
+
 const TextContainer = () => {
   return (
     <div className="flex gap-4 mt-6 lg: gap-8 flex-col lg-flex-row">
@@ -11,7 +13,10 @@ const TextContainer = () => {
         </div>
 
         <div className="relative">
-          <textarea className="textarea"></textarea>
+          <textarea
+            className="textarea"
+            placeholder="Çevirmek istediğiniz metni buraya yazınız"
+          ></textarea>
           <div className="absolute bottom-2 right-2 text-sm text-zinc-500 bg-zinc-100">
             100 karakter
           </div>
@@ -19,8 +24,22 @@ const TextContainer = () => {
       </div>
 
       {/* İkon */}
+      <div className="flex items-center justify-center lg: flex-col ">
+        <div className="size-8 lg:size-12 bg-blue-600 rounded-full grid place-items-center">
+          <ArrowRight className="size-4 lg-size-6 max-lg:rotate-90" />
+        </div>
+      </div>
 
       {/* Çevrilmiş Metin */}
+      <div className="flex-1">
+        <div className="flex items-center justify-between mb-2">
+          <label className="text-sm text-zinc-300">Çevrilmiş Metin</label>
+          <button className="text-xs text-zinc-400 hover:text-zinc-200">
+            Kopyala
+          </button>
+        </div>
+        <textarea className="textarea"></textarea>
+      </div>
     </div>
   );
 };
